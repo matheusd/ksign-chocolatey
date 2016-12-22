b56cbefb77a1adc7c900ce803a499b665f766c8f
Matheus Degiovani <matheus@gigatron.com.br>
2016-12-22 08:46:04 -0200

    First version

---
+++ README.md
@@ -0,0 +1,14 @@
+# Ksign Chocolatey Package
+
+This is package for the community feed of [chocolatey](https://www.chocolatey.org).
+
+This package encapsulates [KSoftware's ksign tool](http://codesigning.ksoftware.net/).
+
+This was made to help automate the setup of developers machine's responsible for
+signing executables and installers.
+
+I am **not** affiliated in any capacity to KSoftware.
+
+I **am** a happy customer of KSoftware certificates. If you need a code signing certificate,
+you can't go wrong with them.
+
---
+++ ksign/tools/chocolateyinstall.ps1
@@ -0,0 +1,41 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'ksign'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://cdn1.ksoftware.net/ksign_installer3.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'ksign*'
+
+  checksum      = '1D962E8AECD3F5B722F986B36298A44AD52E03B1E019D43ED51B63303C1B0416'
+  checksumType  = 'sha256'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
